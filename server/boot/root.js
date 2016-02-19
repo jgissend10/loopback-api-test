f73c3790f286303c87e3a2964637b94835ef08b3
James Gissendaner Jr <JamesTGissendanerJr@gmail.com>
2016-02-19 18:33:16 -0500

    Template

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,15 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+   var loopback = require('loopback');
+
+ //Now setting up the static files..
+ server.use('/static', loopback.static(__dirname + '/../../views/static'));
+
+ // set the view engine to ejs
+ server.set('view engine', 'ejs');
+
+ //Now set the index page for the  '/' route
+ server.get('/', function(req, res) {
+   res.render('index', data);
+ });
+
+}
